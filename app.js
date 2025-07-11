84a746335df4ede69727e5eaec08eef504802063
skrzero <Formationafec@outlook.fr>
2025-07-11 16:56:12 +0200

    exo part 3

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const app = express();
+
+
+
+
+const readJson = (path) => {
+  const raw = fs.readFileSync(path, 'utf-8');
+  return JSON.parse(raw);
+};
+
+const writeJson = (path, data) => {
+  fs.writeFileSync(path, JSON.stringify(data, null, 2));
+};
+
+const afficherFichier = ()=>{
+  const contenu = readJson("data/posts.json");
+  console.log(contenu);
+};
+
 
 app.use(express.json());
 
@@ -12,9 +31,29 @@ app.get('/', (req, res) => {
 });
 
 // Routes à compléter ici
+const postsPath = path.join(__dirname,'data','posts.json');
+app.get('/data/posts',(req,res)=>{
+ const posts = readJson(postsPath);
+  console.log(posts);
+  res.json(posts);
+});
+
+app.get('/data/posts/:id', (req, res) => {
+  const posts = readJson(postsPath);
+  const id = parseInt(req.params.id); 
+
+  const post = posts.find(p => p.id === id); 
+  if (!post) {
+    return res.status(404).json({ error: 'Article non trouvé' });
+  }
+
+  res.json(post);
+});
 
 // Lancement du serveur
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Serveur lancé sur http://localhost:${PORT}`);
 });
+
+
